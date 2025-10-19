e4fd420534afe2a05a192e2e7b86773186d8508b
DaeHunni <daehun0603@gmail.com>
2025-10-19 13:54:54 +0000

    Deploying to gh-pages from @ DaeHunni/DaeHunni.github.io@a5f7a71b4f786104665e74eedef50bb3b423eb3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "News of the Lab",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
